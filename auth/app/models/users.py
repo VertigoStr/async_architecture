2e9bc2a592324fcb93b1dd18e5bbf7412a840a30
Рамиль Мясоутов <ramilmasoutov@MacBook-Pro-Work.local>
2021-11-04 15:12:04 +0300

    feat(auth): add auth service

---
+++ auth/app/models/users.py
@@ -0,0 +1,48 @@
+import enum
+import sqlalchemy
+from sqlalchemy.dialects.postgresql import UUID
+
+metadata = sqlalchemy.MetaData()
+
+class Role(str, enum.Enum):
+    WORKER = 1
+    ACCOUNTANT = 2
+    ADMIN = 3
+
+
+users_table = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("email", sqlalchemy.String(40), unique=True, index=True),
+    sqlalchemy.Column("hashed_password", sqlalchemy.String()),
+    sqlalchemy.Column(
+        "is_active",
+        sqlalchemy.Boolean(),
+        server_default=sqlalchemy.sql.expression.true(),
+        nullable=False,
+    ),
+    sqlalchemy.Column(
+        "role",
+        sqlalchemy.Enum(Role),
+        default=Role.WORKER,
+        nullable=False,
+    ),
+)
+
+
+tokens_table = sqlalchemy.Table(
+    "tokens",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column(
+        "token",
+        UUID(as_uuid=False),
+        server_default=sqlalchemy.text("uuid_generate_v4()"),
+        unique=True,
+        nullable=False,
+        index=True,
+    ),
+    sqlalchemy.Column("expires", sqlalchemy.DateTime()),
+    sqlalchemy.Column("user_id", sqlalchemy.ForeignKey("users.id")),
+)
